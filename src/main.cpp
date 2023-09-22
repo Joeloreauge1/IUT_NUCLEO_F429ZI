d02add7f1c422fb10e81fe153a2c7b9dd6ef240f
Joeloreauge1 <joe.loreau@universite-paris-saclay.fr>
2023-09-22 16:34:41 +0200

    bonjour, je vous depose mon TP3 exo 2 bien a vous

--- src/main.cpp
+++ src/main.cpp
@@ -7,85 +7,48 @@
 
 // Using Arduino pin notation
 C12832 lcd(D11, D13, D12, D7, D10);
-I2C my_i2c(I2C_SDA, I2C_SCL);
-UnbufferedSerial my_serial(USBTX,USBRX,115200);
+
+SPI my_spi(PE_6, PE_5, PE_2, PE_4, use_gpio_ssel);
+
+DigitalOut reset_ligne(PC_6, 0);
+DigitalOut ligne_suivante(PF_8, 1);
+int i = 0;
+void next_line();
 
 int main()
 {
-    char data_mode_stby[2] = {0x07, 0x00};
-    char data_SR[2] = {0x08, 0x21};
-    char data_mode_act[2] = {0x07, 0x01};
 
-    my_i2c.frequency(100000);
+    my_spi.select();
+    my_spi.deselect();
 
-    my_i2c.write(0x98, data_mode_stby, 2, false);
-    my_i2c.write(0x98, data_mode_act, 2, false);
-    my_i2c.write(0x98, data_SR, 2, false);
+    my_spi.frequency(25000000);
+    my_spi.format(8, 0);
 
-    signed char xyz[3];
-    char registre = 0x00;
-    signed char rx_data[3];
-    char buffer[100];
-    bool alert = 0;
-
-
-    my_serial.format(8,SerialBase::None,1);
-
-    while (true)
-    { // this is the third thread
-
-        my_i2c.write(0x98, &registre, 1, true);
-        my_i2c.read(0x99, (char*) rx_data, 3);
-        lcd.cls();
-
-        for (int i = 0; i < 3; i++)
+    char paquet_tx[7][2] =
         {
-            if ((rx_data[i] & 0x20) == 0x20)
-            {
-                xyz[i] = (rx_data[i]) | 0xC0;
-            }
-            else // if ((rx_data[i] & 0x20) == 0x00)
-            {
-                xyz[i] = (rx_data[i]) & 0x3F;
-            }
+            {0x0E, 0x08},
+            {0x11, 0x0C},
+            {0x10, 0x0A},
+            {0x08, 0x09},
+            {0x04, 0x1F},
+            {0x02, 0x08},
+            {0x1F, 0x08},
+        };
+
+    while (1)
+    {
+        reset_ligne = 1;
+        wait_us(1);
+        reset_ligne = 0;
+
+        for (size_t i = 0; i < 7; i++)
+        {
+            my_spi.write(paquet_tx[i], 2, nullptr, 0);
+            ligne_suivante = 0;
+            ThisThread::sleep_for(1ms);
+            ligne_suivante = 1;
         }
 
-        if((xyz[1]& 0x40)== 1)
-        {
-            alert = 1;
-        }
-        
-        else if((xyz[2]& 0x40)== 1)
-        {
-            alert = 1;
-        }
-        
-        if((xyz[3]& 0x40)== 1)
-        {
-            alert = 1;
-        }
-
-
-
-
-        lcd.locate(0, 0);
-        lcd.printf("OUT_X : %.2f\n", static_cast<float>(xyz[0]) * (1.5 / 32.0));
-        lcd.printf("OUT_Y : %.2f\n", static_cast<float>(xyz[1]) * (1.5 / 32.0));
-        lcd.printf("OUT_Z : %.2f\n", static_cast<float>(xyz[2]) * (1.5 / 32.0));
-
-
-
-        if(alert == 1)
-        {
-            lcd.cls();
-            lcd.locate(0,0);
-            lcd.printf("Alert!!!!");
-
-
-        }
-
-        sprintf(buffer," %0.2f\n",static_cast<float>(xyz[1])* (1.5 / 32.0)*720);
-        my_serial.write(buffer,strlen(buffer));
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(3ms);
     }
 }
